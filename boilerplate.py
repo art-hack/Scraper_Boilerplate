5cff4d7091afc875551713bf5c880b1f02f81c5c
Art-hack <17ucs036@lnmiit.ac.in>
2019-05-19 18:52:24 +0530

    Add content

---
+++ boilerplate.py
@@ -0,0 +1,26 @@
+import csv
+from bs4 import BeautifulSoup
+import requests
+
+
+# function to scrape smartybro
+def scrape(string):
+    page_response = requests.get(string, timeout=15)
+    if page_response.status_code == 200:
+        # Do with the response
+    	pass
+    else:
+        print("Error Fetching the Data")
+
+
+# Main driver Program
+
+# Uncomment if you want to clear out the output from the output file everytime you run this
+# listFile2 = open('output.csv', 'w')
+# listFile2.close()
+
+
+with open('input.txt') as openfileobject:
+    for line in openfileobject:
+        page_link = line
+        scrape(page_link)
